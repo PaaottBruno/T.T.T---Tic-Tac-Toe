d04734f34422ed29b7764f50d61512956b17d7e9
jotafreitas <132005298+PaaottBruno@users.noreply.github.com>
2024-02-07 17:32:31 -0400

    foi feito a troca de tela
    tenho que arrumar a ajustar o jogo

--- Kivy/main.py
+++ Kivy/main.py
@@ -1,49 +1,18 @@
+import kivy
+from pages.home import Home
+from pages.game import Game
+
 from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.gridlayout import GridLayout
-from functools import partial
-
-class TicTacToe(GridLayout):
-    def __init__(self, **kwargs):
-        super(TicTacToe, self).__init__(**kwargs)
-        self.cols = 3
-        self.buttons = []
-        self.current_player = 'X'
-        self.board = [[' ' for _ in range(3)] for _ in range(3)]
-
-        for row in range(3):
-            for col in range(3):
-                button = Button(text='', font_size=40, on_press=partial(self.on_button_press, row, col))
-                self.add_widget(button)
-                self.buttons.append(button)
-
-    def on_button_press(self, row, col, button):
-        if self.board[row][col] == ' ':
-            self.board[row][col] = self.current_player
-            button.text = self.current_player
-            if self.check_winner():
-                print(f'{self.current_player} wins!')
-                self.reset_board()
-            else:
-                self.current_player = 'O' if self.current_player == 'X' else 'X'
-                self.minimax_move()
-
-    def check_winner(self):
-        # Implement your winning condition check here
-        # Return True if there is a winner, False otherwise
-        pass
-
-    def reset_board(self):
-        # Implement board reset logic here
-        pass
-
-    def minimax_move(self):
-        # Implement Minimax algorithm to make the computer's move
-        pass
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 class TicTacToeApp(App):
     def build(self):
-        return TicTacToe()
 
-if __name__ == '__main__':
-    TicTacToeApp().run()
+        sm = ScreenManager()
+        sm.add_widget(Home(name = "home"))
+        sm.add_widget(Game(name = "game"))
+        sm.current = "home"
+
+        return sm
+    
+TicTacToeApp().run()
