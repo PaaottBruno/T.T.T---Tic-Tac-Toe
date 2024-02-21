819b8f8d2b78a4d91eec01e8cf1c7de374ef8fba
Javador48 <paaott08@gmail.com>
2024-02-21 01:58:13 -0400

    Ajustando o codigo par afuncionar na tela menu

--- menu.py
+++ menu.py
@@ -2,9 +2,10 @@ from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Button
 from kivy.uix.image import Image
-from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.screenmanager import Screen
 from kivy.uix.textinput import TextInput
-
+from kivy.uix.label import Label
+from kivy.graphics import Line, Color
 
 class Menu(Screen):
     def __init__(self, **kwargs):
@@ -12,10 +13,15 @@ class Menu(Screen):
 
         layout = FloatLayout()
 
-        # Adiciona a imagem de fundo ao layout
-        image_pag_home = Image(source='img_pagName_player.png', allow_stretch=True, keep_ratio=False)
-        layout.add_widget(image_pag_home)
+        # Adiciona o layout à tela
+        self.add_widget(layout)
+        layout = FloatLayout()
 
+        # Imagem de fundo
+        background = Image(source='pagina_inicialMobile.png', allow_stretch=True, keep_ratio=False)
+        layout.add_widget(background)
+        
+        # Botão voltar
         button_seta_voltar = Button(size_hint=(None, None),
                                     pos_hint={'center_x': 0.08, 'center_y': 0.95},
                                     size=(120, 85),
@@ -23,40 +29,48 @@ class Menu(Screen):
                                     background_down='seta-voltar.png')
         button_seta_voltar.bind(on_press=self.on_button_press_voltar)
         layout.add_widget(button_seta_voltar)
-
-        # Cria um TextInput com caixa de texto invisível e letras visíveis
-        name_player_1 = TextInput(hint_text='Name Player 1',
-                                  background_color=(1, 1, 1, 0),
-                                  foreground_color=(1, 1, 1, 1),
-                                  multiline=False, size_hint=(None, None), size=(410, 80),
-                                  pos_hint={'center_x': 0.5, 'center_y': 0.6},
-                                  border=(0, 0, 0, 0),
-                                  font_size=40)
-        layout.add_widget(name_player_1)
-
-        name_player_2 = TextInput(hint_text='Name Player 2',
-                                  background_color=(1, 1, 1, 0),
-                                  foreground_color=(1, 1, 1, 1),
-                                  multiline=False, size_hint=(None, None), size=(410, 80),
-                                  pos_hint={'center_x': 0.5, 'center_y': 0.4},
-                                  border=(0, 0, 0, 0),
-                                  font_size=40)
-        layout.add_widget(name_player_2)
-
-        image_linha = Image(source='img_line_player.png', allow_stretch=True, keep_ratio=True,
-                            size_hint=(0.5, 0.5), pos_hint={'center_x': 0.5, 'center_y': 0.5})
-        layout.add_widget(image_linha)
+        
+        # Titulo da pagina
+        msg_ajuda = Label(text = "[b]Escolha seu proprio nome[b]",
+                          markup=True,
+                          pos_hint={'center_x': 0.5, 'center_y': 0.70},
+                          font_size=90)
+        layout.add_widget(msg_ajuda)
+        
+        # Adicionar um TextInput ao layout
+        self.text_input_01 = TextInput(multiline=False, 
+                                       hint_text='Name Player 1',
+                                       size_hint=(None, None),
+                                       pos_hint={'center_x': 0.5, 'center_y': 0.59},
+                                       size=(250, 40), 
+                                       background_color=(0, 0, 0, 0))
+        layout.add_widget(self.text_input_01)
+        
+        # Adicionar um TextInput ao layout
+        self.text_input_02 = TextInput(multiline=False, 
+                                       hint_text='Name Player 2',
+                                       size_hint=(None,None),
+                                       pos_hint={'center_x': 0.5, 'center_y': 0.47},
+                                       size=(250, 40), 
+                                       background_color=(0, 0, 0, 0))
+        layout.add_widget(self.text_input_02)  
+        with layout.canvas:
+            Color(0, 0, 0)
+            # Adicionando uma linha ao layout
+            line2 = Line(points=[280, 350, 450, 350], width=1,)  # Pontos são coordenadas x, y
+                
+            # Adicionando uma linha ao layout
+            line1 = Line(points=[280, 278, 450, 278], width=1,)  # Pontos são coordenadas x, y
 
         # Adicionar botão de imagem sobre a imagem de fundo
-        button_play = Button(background_normal='img_btnPlay_home.png',
+        btn_play = Button(background_normal='img_btnPlay_home.png',
                              background_down='img_btnPlay_home.png',
-                             size_hint=(None, None),
-                             pos_hint={'center_x': 0.5, 'center_y': 0.1},
+                             size_hint=(None,None),
+                             pos_hint={'center_x': 0.5, 'center_y': 0.27},
                              size=(600, 100))
-        button_play.bind(on_press=self.on_button_press_play)
-        layout.add_widget(button_play)
-
-        # Adiciona o layout à tela
+        btn_play.bind(on_press=self.on_button_press_play)
+        layout.add_widget(btn_play)
+        
         self.add_widget(layout)
 
     def on_button_press_voltar(self, instance):
